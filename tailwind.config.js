9b9462dd85ce34d8dd6f63992501864484780f93
ChurrinChurron <marianafantasy8@gmail.com>
2022-06-26 15:01:31 -0400

    dropdown-navigation-section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      'almostwhite': 'hsl(0, 0%, 98%)',
+      'mediumgray': 'hsl(0, 0%, 41%)',
+      'almostblack': 'hsl(0, 0%, 8%)',
+      'white': 'hsl(0, 0%, 100%)',
+    },
+    fontFamily: {
+      epilogue: ['Epilogue', 'sans-serif'],
+    },
+    extend: {
+      width: {
+        '30': '30rem',
+        '32': '32rem',
+        '34': '34rem',
+      }
+    },
+  },
+  plugins: [],
+}
